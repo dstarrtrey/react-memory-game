0ec9f046968d137c5d401fd8a4720e55d78e05c2
dstarrtrey <dstarrtrey@gmail.com>
2019-05-03 14:23:10 -0700

    Updates

---
+++ precache-manifest.3913f9c3a75b929e9a7f6955a67280ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568b897bf24f648c374cc1060b0b691e",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "bb02179715864426a669",
+    "url": "/react-memory-game/static/js/2.bbe9a722.chunk.js"
+  },
+  {
+    "revision": "38d04663cd05f5fff265",
+    "url": "/react-memory-game/static/js/main.61dcd2f1.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  }
+]);
